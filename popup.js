5e59e2f063be24f5daeb01e7c0e6cdcf47fe2164
LS <sassinlucke@gmail.com>
2025-09-04 10:45:33 +1200

    Create popup.js

---
+++ popup.js
@@ -0,0 +1,8 @@
+document.getElementById('embedButton').addEventListener('click', () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.scripting.executeScript({
+      target: { tabId: tabs[0].id },
+      files: ['content.js']
+    });
+  });
+});
